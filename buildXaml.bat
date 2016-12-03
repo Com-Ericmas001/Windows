bcf0accbbbf0968b8d9eb0863fefb3687130d917
Éric Massé <Ericmas001@gmail.com>
2016-12-03 13:04:51 -0500

    v0.3

---
+++ buildXaml.bat
@@ -0,0 +1,24 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+call %NuGet% restore Windows.sln -NonInteractive
+msbuild Windows.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net45
+
+%nuget% pack "Xaml.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
