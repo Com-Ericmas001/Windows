2044829327892bfdae09401cbc4a1945a9db72c8
Éric Massé <Ericmas001@gmail.com>
2017-01-07 08:56:14 -0500

    v 0.4.3: NumericUpDown from https://denisdoucet.visualstudio.com/Apps/_versionControl?path=%24%2FApps%2FMain%2FCustomControls

---
+++ Com.Ericmas001.Windows.Xaml/CustomControls/NumericUpDown.xaml.cs
@@ -0,0 +1,88 @@
+﻿// From https://denisdoucet.visualstudio.com/Apps/_versionControl?path=%24%2FApps%2FMain%2FCustomControls%2FControls%2FNumericUpDown.xaml.cs
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Com.Ericmas001.Windows.Xaml.CustomControls
+{
+    public partial class NumericUpDown : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(decimal), typeof(NumericUpDown), new PropertyMetadata(0m));
+
+        public decimal Value
+        {
+            get { return (decimal)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinimumProperty = DependencyProperty.Register("Minimum", typeof(decimal), typeof(NumericUpDown), new PropertyMetadata(0m));
+
+        public decimal Minimum
+        {
+            get { return (decimal)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty = DependencyProperty.Register("Maximum", typeof(decimal), typeof(NumericUpDown), new PropertyMetadata(100m));
+
+        public decimal Maximum
+        {
+            get { return (decimal)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public static readonly DependencyProperty IncrementProperty = DependencyProperty.Register("Increment", typeof(decimal), typeof(NumericUpDown), new PropertyMetadata(1m));
+
+        public decimal Increment
+        {
+            get { return (decimal)GetValue(IncrementProperty); }
+            set { SetValue(IncrementProperty, value); }
+        }
+
+        public static readonly DependencyProperty DecimalPlacesProperty = DependencyProperty.Register("DecimalPlaces", typeof(int), typeof(NumericUpDown), new PropertyMetadata(0));
+
+        public int DecimalPlaces
+        {
+            get { return (int)GetValue(DecimalPlacesProperty); }
+            set { SetValue(DecimalPlacesProperty, value); }
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"[^0-9\-\.]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void textBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            decimal val = Value;
+            var str = textBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(str))
+            {
+                val = Minimum;
+            }
+
+            Regex regex = new Regex(@"^-?\d+\.?\d*$");
+            if (regex.IsMatch(str))
+            {
+                val = Convert.ToDecimal(str);
+
+                val = Math.Min(val, Maximum);
+                val = Math.Max(val, Minimum);
+
+                val = Math.Round(val / Increment, MidpointRounding.AwayFromZero) * Increment;
+            }
+
+            textBox.Text = val.ToString(string.Concat("F", DecimalPlaces), CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Com.Ericmas001.Windows/Converters/NumericUpDowValueToString.cs
@@ -0,0 +1,30 @@
+﻿//From https://denisdoucet.visualstudio.com/Apps/_versionControl?path=%24%2FApps%2FMain%2FCustomControls%2FConverter%2FNumericUpDowValueToString.cs
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Com.Ericmas001.Windows.Converters
+{
+    public class NumericUpDowValueToString : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var value = values[0] as decimal?;
+            var decimalPlaces = values[1] as int?;
+
+            if (value != null && decimalPlaces != null)
+            {
+                return ((decimal)value).ToString(string.Concat("F", decimalPlaces), CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                return "0";
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[] { System.Convert.ToDecimal((string)value) };
+        }
+    }
+}
